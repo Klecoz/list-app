2c26c76c9e4196f77b0b0188ed24229cbe0e7faa
Klecoz <arseniocolon@gmail.com>
2015-05-26 16:52:13 -0400

    Added append() to add a list item using the textbox input.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-var listAdd = 'defaultValue';
+//defines text box variable.
+ var listAdd;
 
 $( document ).ready(function() {
 
-    $('#listItem').keydown(function (event) {
-  if (event.which == 13) {
+  $('#listItem').keydown(function (event) {
+    if (event.which == 13) {
+
     //Prevents default function of key.
     event.preventDefault();
 
@@ -13,6 +15,10 @@ $( document ).ready(function() {
     //Test to show that variable has been changed.
     console.log('success!');
     console.log(listAdd);
+
+
+    //Uses .append() to add a list item to the end of the list.
+    $('.listsActual').append('<li class="notDone">' + listAdd + '</li>');
   }
 
 });
